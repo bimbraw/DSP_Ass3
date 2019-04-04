4954e06ae2df866e9d6a4ab4837baeb1a7488d58
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2019-04-03 23:51:14 -0400

    Update ass3main.m

--- ass3main.m
+++ ass3main.m
@@ -48,8 +48,11 @@ title('Magnitude Spectrum for hp')
 %--------
 %Q3 Pwelch
 x = audioread('sine-440-96k.wav');
+%sound(x,96000);
 y = myNoiseShape(x,4,'tri');
+%sound(y,96000);
 e = y - x;
+%sound(e,96000)
 figure;
 pwelch(e);
 
@@ -63,26 +66,31 @@ title('Magnitude Spectrum, w = 4, Tri on Sine')
 %Pwelch Q.4
 x = audioread('flute-A4-96k.wav');
 y_q_4 = myQuantize(x,4);
+%sound(y_q_4,96000)
 figure;
 pwelch(y_q_4)
 y_q_8 = myQuantize(x,8);
+%sound(y_q_8,96000)
 figure;
 pwelch(y_q_8)
 
 y_d_4 = myDither(x,4,'hp');
+%sound(y_d_4,96000)
 figure;
 pwelch(y_d_4)
 y_d_8 = myDither(x,8,'hp');
+%sound(y_d_8,96000)
 figure;
 pwelch(y_d_8)
 
 y_n_4 = myNoiseShape(x,4,'hp');
+sound(y_n_4,96000)
 figure;
 pwelch(y_n_4)
 y_n_8 = myNoiseShape(x,8,'hp');
+%sound(y_n_8,96000)
 figure;
 pwelch(y_n_8)
-
 
 %Magnitude Spectrum - Q.4
 subplot(3,2,1)
